33ffa39e2d7eaa19a4a43b7f432c76cea5d23047
lucas-wa <lucas.alves2@discente.ufg.br>
2025-06-20 14:27:09 -0300

    Adding exercices

--- devops/scripts/llama2-7b-chat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-vllm serve meta-llama/Llama-2-7b-chat-hf --max-model-len=752
--- devops/scripts/ngrok.sh
+++ devops/scripts/ngrok.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ngrok config add-authtoken $NGROK_TOKEN
+ngrok http $PORT --auth-token $NGROK_TOKEN
